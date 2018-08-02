d57d978cf4689edb0d44de30ef7d6ce9ea63a5c7
baschingz <36448854+baschingz@users.noreply.github.com>
2018-08-02 20:57:45 +0700

    Initial commit

---
+++ Controllers/CandidateController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommunityWeb.Controllers
+{
+    public class CandidateController : Controller
+    {
+        public IActionResult ShowCandidate() {
+            return View();
+        }
+        public IActionResult InsertCandidate()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/ShowCandidateController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("myapp", []);
+
+app.controller("ShowCandidateController", function ($scope, $http) {
+    $scope.employees = typeof employees != "undefined" ? employees ? employees : null : null;
+    $scope.init = function () {
+        //debugger;
+        $http.get("http://localhost:3002/api/generateuser").then(function (response) {
+
+            if (response.data) {
+                //debugger;
+                alert('ดึงรายชื่อสำเร็จ');
+                $scope.employees = response.data;
+            } else {
+                alert('ดึงรายชื่อไม่สำเร็จ');
+            }
+        }, function (response) {
+            debugger;
+            alert('fail');
+        });
+    }
+});
